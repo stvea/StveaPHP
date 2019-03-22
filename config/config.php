12597ac70e0f63672cca055ebc52821937ea5cd5
unknown <1191743792@qq.com>
2019-03-22 10:27:25 +0800

    first

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php 
+	// 数据库配置
+	$config['db']['host'] = '';
+	$config['db']['username'] = '';
+	$config['db']['password'] = '';
+	$config['db']['dbname'] = '';
+	// 默认控制器和操作名
+	$config['defaultController'] = '';
+	$config['defaultAction'] = '';
+	$config['defaultColor'] = '';
+	return $config;
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+// 应用目录为当前目录
+define('APP_PATH', __DIR__ . '/');
+
+// 开启调试模式
+define('APP_DEBUG', true);
+
+// 加载框架文件
+require(APP_PATH . 'stveaphp/Stveaphp.php');
+
+// 加载配置文件
+$config = require(APP_PATH . 'config/config.php');
+
+session_start();
+
+// 实例化框架类
+(new stveaphp\Stveaphp($config))->run();
